2190dc096c7d0c15bd7bc57ff629ce1c44c28ecf
kykil <kilkiyoung@gmail.com>
2021-01-13 14:43:02 +0900

    [CSS][JS] scroll section
    - 스크롤 처리를 위해 활성되고 있는 section에 대한 정보처리 내용 추가.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //자동 호출 함수
 (() => {
   let yOffset = 0;
+  let prevScrollHeight = 0; //현재 스크롤 위치보다 이전 위치한 스크롤 섹션들의 높이 합
+  let currentScene = 0;
 
   const sceneInfo = [
     {
@@ -45,20 +47,49 @@
     //각 스크롤 섹션의 높이 세팅
     for(let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[i].objs.container.style.height = '${sceneInfo[i].scrollHeight}px';
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
+
+    let totalScrollHeight = 0;
+    for(let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if(totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
   function scrollLoop() {
-    
-  }
+    prevScrollHeight = 0;
 
-  window.addEventListener('resize', setLayout);
+    for(let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+
+    if(yOffset < prevScrollHeight) {
+      if(currentScene == 0) return;
+      currentScene--;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+  }
+  
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
 
-  setLayout();
+  //window.addEventListener('DOMContentLoaded', setLayout);
+  window.addEventListener('load', setLayout);
+  window.addEventListener('resize', setLayout);
 
 })();
